9f61647b8362ff3a3efae6393cff774153a9823b
Guillermo Lopez <omar10594@hotmail.com>
2021-05-05 02:50:04 -0500

    Fix prefix

--- nanachi-dj/index.js
+++ nanachi-dj/index.js
@@ -45,7 +45,7 @@ client.on("message", async (message) => {
   if (message.author.bot) return;
   if (!message.guild) return;
 
-  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(PREFIX)})\\s*`);
+  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(DISCORD_PREFIX)})\\s*`);
   if (!prefixRegex.test(message.content)) return;
 
   const [, matchedPrefix] = message.content.match(prefixRegex);
